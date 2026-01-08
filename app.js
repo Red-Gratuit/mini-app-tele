ff16661a93c29ff288c3f78babdaa9652e5e9edc
enzo <enzo.xr59@gmail.com>
2026-01-08 23:58:30 +0100

    XRshop premium intro identical to video

--- app.js
+++ app.js
@@ -9,29 +9,26 @@ const content = document.getElementById("content");
 
 if (tg) tg.expand();
 
-// 🎚️ Fade-in musique
 function fadeInMusic() {
   music.volume = 0;
-  music.play().catch(() => {});
+  music.play().catch(()=>{});
   let v = 0;
-  const fade = setInterval(() => {
+  const i = setInterval(() => {
     if (v < 0.5) {
       v += 0.02;
       music.volume = v;
-    } else clearInterval(fade);
+    } else clearInterval(i);
   }, 80);
 }
 
-// ▶️ Choisir sa puff
-startBtn.addEventListener("click", () => {
+startBtn.onclick = () => {
   fadeInMusic();
   intro.classList.remove("active");
   content.classList.add("active");
-});
+};
 
-// 👻 Snapchat
-snapBtn.addEventListener("click", () => {
+snapBtn.onclick = () => {
   const url = "https://www.snapchat.com/add/enzoxr.59";
   if (tg) tg.openLink(url);
   else window.open(url, "_blank");
-});
+};
